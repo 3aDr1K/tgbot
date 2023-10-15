712af288a604a54e88ef50f04a0caf730eb63d4c
Evgenii <velsh.yakimenko@icloud.com>
2023-10-15 19:08:51 +0300

    -

--- src/main/java/com/example/tgbot/TgbotApplication.java
+++ src/main/java/com/example/tgbot/TgbotApplication.java
@@ -2,17 +2,12 @@ package com.example.tgbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import javax.annotation.Resource;
 
 @SpringBootApplication
 @EnableScheduling
 public class TgbotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TgbotApplication.class, args);
 	}
-
 }
