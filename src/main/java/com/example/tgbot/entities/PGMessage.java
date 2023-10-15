3d3465f7de140e42990ea3ac81440f6245670d79
Evgenii <velsh.yakimenko@icloud.com>
2023-10-15 17:28:57 +0300

    The commented-out code was removed

--- src/main/java/com/example/tgbot/entities/PGMessage.java
+++ src/main/java/com/example/tgbot/entities/PGMessage.java
@@ -23,37 +23,4 @@ public class PGMessage {
     private Timestamp timestamp;
 
     private String botResponse;
-/*
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Timestamp getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Timestamp timestamp) {
-        this.timestamp = timestamp;
-    }
-*/
 }
