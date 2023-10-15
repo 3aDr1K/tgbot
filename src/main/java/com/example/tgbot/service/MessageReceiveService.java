19dfee3c40a42072c9e41b280c89f59e9a9d4353
Evgenii <velsh.yakimenko@icloud.com>
2023-10-15 19:57:55 +0300

    Added date to response

--- src/main/java/com/example/tgbot/service/MessageReceiveService.java
+++ src/main/java/com/example/tgbot/service/MessageReceiveService.java
@@ -6,6 +6,8 @@ import com.example.tgbot.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -17,14 +19,15 @@ public class MessageReceiveService {
 
     @Autowired
     private MyTelegramBot telegramBot;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     public void sendRegistrationMessageToUsers() {
         List<User> registeredUsers = userRepository.findAll(); // Получаем всех зарегистрированных пользователей
         long totalDomains = dailyDomainRepository.count(); // Получаем общее количество доменов
-
+        Date currentDate = new Date();
         for (User user : registeredUsers) {
             long chatId = user.getChatId();
-            String message = String.format("Зарегистрировано %d доменов", totalDomains);
+            String message = String.format("(%s) Зарегистрировано %d доменов", dateFormat.format(currentDate), totalDomains);
             // Отправляем сообщение каждому пользователю
             telegramBot.sendMessage(chatId, message);
         }
